8a1dcb99c08f45459ad5e3b61c49aed084965cfa
korotyshev-intropro <ikorotyshev@intropro.com>
2016-09-19 17:23:25 +0300

    Utils file

---
+++ main/java/ua/vzaperi/matrix/util/MatrixLoaderEvents.java
@@ -0,0 +1,19 @@
+package ua.vzaperi.matrix.util;
+
+/**
+ * Events for the MatrixLoader application. 
+ * GUI will listen for incoming actions and react somehow on these events. 
+ *
+ */
+public enum MatrixLoaderEvents {
+	NOP, // No operation
+	RESET, // Game reset command - set everything into initial state
+	NEO_CONNECTED, // Neo wire connected
+	NEO_DISCONNECTED,
+	DISK1_CONNECTED, // Disk 1 inserted into the drive
+	DISK1_DISCONNECTED,
+	DISK2_CONNECTED,
+	DISK2_DISCONNECTED,
+	DISK3_CONNECTED,
+	DISK4_DISCONNECTED,
+}
