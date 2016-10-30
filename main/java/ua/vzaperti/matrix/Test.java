82ec3bd2530018c42c2ab184ba5dbeda556d61e1
korotyshev <korotyshev@gmail.com>
2016-10-30 13:30:12 +0200

    [+] VideoPanel class

---
+++ main/java/ua/vzaperti/matrix/Test.java
@@ -0,0 +1,56 @@
+package ua.vzaperti.matrix;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+
+import ua.vzaperti.util.VideoPanel;
+
+public class Test extends JFrame {
+
+	private static final long serialVersionUID = -6671518318930567262L;
+	private JPanel contentPane;
+	private ua.vzaperti.util.VideoPanel vp; 
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Test frame = new Test();
+					frame.setVisible(true);
+					frame.start();
+
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	protected void start() {
+		vp.start();		
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public Test() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		contentPane.setLayout(new BorderLayout(0, 0));
+		setContentPane(contentPane);
+		
+		vp = new VideoPanel("C:/ivms4200/video/2.mp4");
+
+		contentPane.add(vp, BorderLayout.CENTER);
+	}
+
+}
